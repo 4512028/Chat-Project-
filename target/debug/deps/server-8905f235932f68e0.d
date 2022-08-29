318e9d78f9848f98dfc597562c56d22302532b2e
umer saleem <mumersaleem79@gmail.com>
2022-08-29 10:59:11 +0500

    updated

---
+++ target/debug/deps/server-8905f235932f68e0.d
@@ -0,0 +1,5 @@
+/Users/invozone/Documents/umer/Rust/rustProject/Rust_client-server_chat-master/chat/server/target/debug/deps/server-8905f235932f68e0: src/main.rs
+
+/Users/invozone/Documents/umer/Rust/rustProject/Rust_client-server_chat-master/chat/server/target/debug/deps/server-8905f235932f68e0.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/server.d
@@ -0,0 +1,1 @@
+/Users/invozone/Documents/umer/Rust/rustProject/Rust_client-server_chat-master/chat/server/target/debug/server: /Users/invozone/Documents/umer/Rust/rustProject/Rust_client-server_chat-master/chat/server/src/main.rs
